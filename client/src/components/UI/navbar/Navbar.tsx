6198411a3462117a912dce93ea0fb00a47e2d307
Alba Romero <albaromero22@gmail.com>
2023-10-05 23:28:57 +0200

    refactor: modify navbar based on feedback

--- client/src/components/UI/navbar/Navbar.tsx
+++ client/src/components/UI/navbar/Navbar.tsx
@@ -1,69 +1,14 @@
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
 import { Layout, Input } from "antd";
-import Overlay from "../overlay/Overlay";
+import { IconBookmark } from "@tabler/icons-react";
+
 import Avatar from "../avatar/Avatar";
-import {
-  IconBookmark,
-  IconUser,
-  IconLogout,
-  IconMenu2,
-} from "@tabler/icons-react";
 
 import logo from "../../../media/shift-logo-morado.png";
 
 import "./Navbar.css";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
   const { Search } = Input;
-
-  const logout = () => {
-    // Send logout request to the backend
-    axios
-      .get("/api/logout", { withCredentials: true })
-      .then((response) => {
-        // Handle the backend response here
-        console.log("Backend Response:", response.data);
-        console.log("Response", response.status);
-        navigate("/login");
-      })
-      .catch((error) => {
-        // Handle any errors here
-        if (error.response) {
-          console.error("HTTP Status Code:", error.response.status);
-          console.error("Response Data:", error.response.data);
-        } else {
-          console.error("Network Error:", error.message);
-        }
-      });
-  };
-
-  const profileOverlay = (
-    <>
-      <div className="options">
-        <IconUser color="var(--color-1)" /> Profile
-      </div>
-      <div className="options" onClick={logout}>
-        <IconLogout color="var(--color-1)" /> Logout
-      </div>
-    </>
-  );
-
-  const mobileProfileOverlay = (
-    <>
-      <div className="options">
-        <IconUser color="var(--color-1)" /> Profile
-      </div>
-      <div className="options">
-        <IconBookmark color="var(--color-1)" /> Short List
-      </div>
-      <div className="options" onClick={logout}>
-        <IconLogout color="var(--color-1)" /> Logout
-      </div>
-    </>
-  );
 
   return (
     <nav className="navbar">
@@ -86,21 +31,9 @@ const Navbar = () => {
 
             <div className="rightMenu">
               <IconBookmark color="var(--color-1)" />
-              <Overlay
-                icon={
-                  //TODO: update names with login profile
-                  <Avatar firstName="Laura" lastName="Purcaro" size="large" />
-                }
-                content={profileOverlay}
-              />
+              {/* TODO: update names with login profile */}
+              <Avatar firstName="Laura" lastName="Purcaro" size="large" />
             </div>
-          </div>
-
-          <div className="mobileMenu">
-            <Overlay
-              icon={<IconMenu2 color="var(--color-1)" />}
-              content={mobileProfileOverlay}
-            />
           </div>
         </Layout.Header>
       </Layout>
--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes as Routing, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes as Routing, Route } from "react-router-dom";
 
-import Login from '../components/pages/Login';
-import Homepage from '../components/pages/Homepage';
+import Login from "../components/pages/Login";
+import Homepage from "../components/pages/Homepage";
 
-export interface IApplicationProps {};
+export interface IApplicationProps {}
 
 const Routes: React.FC<IApplicationProps> = (props) => {
-
-    return (
-        <BrowserRouter>
-            <Routing>
-                <Route path='/' element={<Homepage/>}/>
-                <Route path='/login' element={<Login/>}/>
-            </Routing>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Routing>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routing>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
