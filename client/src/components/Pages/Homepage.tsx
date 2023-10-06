74d780ea4abf1b55ee01b73e2ca1a3a2253532d5
laurapurcarogmail <laurapurcaro@gmail.com>
2023-10-06 19:42:32 +0200

    added logout functionality

--- client/src/components/Pages/Homepage.tsx
+++ client/src/components/Pages/Homepage.tsx
@@ -6,6 +6,8 @@ import { DashboardCandidate } from "./DashboardCandidate";
 import { Companies } from "./Companies";
 import { Jobs } from "./Jobs";
 import { Saved } from "./Saved";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const { Header, Footer, Sider, Content } = Layout;
 
@@ -39,8 +41,67 @@ const footerStyle: React.CSSProperties = {
   backgroundColor: "#7dbcea",
 };
 
+// Define a type for the result object
+interface LogoutResult {
+  success: boolean;
+  data: any | null;
+  error: {
+    status: number;
+    data: any;
+  } | {
+    message: string;
+  } | null;
+}
+
+const logout = (): LogoutResult => {
+  // Create an object to store the result
+  let result: LogoutResult = {
+    success: false,
+    data: null,
+    error: null,
+  };
+
+  // Send logout request to the backend (you'll need to replace the URL and method)
+  axios
+    .post("/api/logout", { withCredentials: true })
+    .then((response) => {
+      // Handle the backend response here
+      console.log("Backend Response:", response.data);
+      console.log("Response", response.status);
+
+      // Update the result object with the response data
+      result.success = true;
+      result.data = response.data;
+    })
+    .catch((error) => {
+      // Handle any errors here
+      if (error.response) {
+        console.error("HTTP Status Code:", error.response.status);
+        console.error("Response Data:", error.response.data);
+
+        // Update the result object with the error information
+        result.error = {
+          status: error.response.status,
+          data: error.response.data,
+        };
+      } else {
+        console.error("Network Error:", error.message);
+
+        // Update the result object with the network error
+        result.error = {
+          message: error.message,
+        };
+      }
+    });
+
+  // Return the result object
+  return result;
+};
+
+
 const LayoutC = () => {
   const [selectedComponent, setSelectedComponent] = useState("dashboard");
+  const navigate = useNavigate();
 
   // Use a switch statement or if/else statements to determine which component to render
   let componentToRender;
@@ -57,6 +118,10 @@ const LayoutC = () => {
       break;
     case "saved":
       componentToRender = <Saved />;
+      break;
+    case "logout":
+      logout();
+      navigate("/login");
       break;
     default:
       componentToRender = (
