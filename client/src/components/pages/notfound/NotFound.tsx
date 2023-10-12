dacc93ca82fe4f114ea4de55d2526424d7478920
laurapurcarogmail <laurapurcaro@gmail.com>
2023-10-12 17:40:35 +0200

    not found page

--- client/src/components/pages/notfound/NotFound.tsx
+++ client/src/components/pages/notfound/NotFound.tsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import styling from "./NotFound.module.css";
+import image from "../../../media/not-found.svg";
 
 interface NotFoundProps {
   link?: string;
 }
 
 const NotFound: React.FC<NotFoundProps> = ({ link }) => {
-
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className={styling.notFound}>
+      <div className={styling.notFoundContent}>
+        <h1>404 - Page Not Found</h1>
+        <p>
+          Oops! It seems like you've taken a wrong turn in your job search
+          journey.
+        </p>
+        <p>Don't worry, we'll help you get back on track!</p>
+        <img src={image} alt="Lost Job Seeker" />
+      </div>
     </div>
   );
 };
