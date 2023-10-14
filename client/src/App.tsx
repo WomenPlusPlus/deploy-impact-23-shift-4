3a30e58e946779b12a264e032b77a855343d5049
Alba Romero <albaromero22@gmail.com>
2023-10-14 20:46:49 +0200

    resolve conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,19 +2,22 @@ import "./App.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ConfigProvider } from "antd";
 import Routes from "./routes/routes";
+import { AuthProvider } from "./context/auth";
 
 function App() {
   return (
     <GoogleOAuthProvider clientId="<your_client_id>">
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: "#10239e",
-          },
-        }}
-      >
-        <Routes />
-      </ConfigProvider>
+      <AuthProvider>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#10239e",
+            },
+          }}
+        >
+          <Routes />
+        </ConfigProvider>
+      </AuthProvider>
     </GoogleOAuthProvider>
   );
 }
