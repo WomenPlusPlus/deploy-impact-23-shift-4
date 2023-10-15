82d8a8922fd946a68eb4ee77d1da5fd2f7aaedfe
Alba Romero <albaromero22@gmail.com>
2023-10-15 19:43:10 +0200

    update style

--- client/src/components/pages/dashboardCompanies/DashboardCompanies.tsx
+++ client/src/components/pages/dashboardCompanies/DashboardCompanies.tsx
@@ -1,4 +1,4 @@
-import styling from "./DashboardCompanies.module.css";
+import { useNavigate } from "react-router-dom";
 import { ProgressBar } from "../../UI/progressbar/ProgressBar";
 import { IconExternalLink } from "@tabler/icons-react";
 import { Button } from "../../UI/button/Button";
@@ -6,9 +6,13 @@ import { HorizontalCard } from "../../UI/card/HorizontalCard";
 import { CardContainer } from "../../UI/container/CardContainer";
 import Avatar from "../../UI/avatar/Avatar";
 
+import styling from "./DashboardCompanies.module.css";
+
 const DashboardCompany = () => {
   const company_name = "Dream Company";
   const location = "New York, NY";
+
+  const navigate = useNavigate();
 
   const progress = 80;
   return (
@@ -22,7 +26,11 @@ const DashboardCompany = () => {
           <p>{location}</p>
         </div>
 
-        <IconExternalLink color="black" />
+        <IconExternalLink
+          className={styling.icon}
+          color="black"
+          onClick={() => navigate("/company-profile")}
+        />
       </CardContainer>
 
       {/* Progress bar */}
