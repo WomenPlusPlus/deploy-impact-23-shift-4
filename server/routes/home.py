7f9612fd4067d3e94aafed5182da91f1b6fd33da
laurapurcarogmail <laurapurcaro@gmail.com>
2023-10-15 11:21:16 +0200

    fix error

--- server/routes/home.py
+++ server/routes/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 
-home_bp = Blueprint("add_value", __name__)
+home_bp = Blueprint("homepage", __name__)
 
 @home_bp.route("/", methods=["GET"])
 def homepage():
