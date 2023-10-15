d47d673e3da31fc65789cdc27974b65eea3eaa6f
Alba Romero <albaromero22@gmail.com>
2023-10-15 23:24:12 +0200

    update axios import logout

--- client/src/components/layout/sidebar/Sidebar.tsx
+++ client/src/components/layout/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+import configureAxios from "../../../config";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Layout as AntLayout, Menu } from "antd";
@@ -14,6 +14,7 @@ import {
 } from "@tabler/icons-react";
 
 import "./Sidebar.css";
+const axios = configureAxios();
 
 const { Sider } = AntLayout;
 
