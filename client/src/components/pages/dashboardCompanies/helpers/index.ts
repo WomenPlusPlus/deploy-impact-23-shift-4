79108b95e8f1d30ef89f37e2daab0752bb995df8
Alba Romero <albaromero22@gmail.com>
2023-10-22 23:18:11 +0200

    fix problems

--- client/src/components/pages/dashboardCompanies/helpers/index.ts
+++ client/src/components/pages/dashboardCompanies/helpers/index.ts
@@ -2,23 +2,23 @@
 export const getMatchingCandidatesInfo = (jobData: any, candidateData: any) => {
   const resultArray: any = [];
 
-  jobData.forEach((job: any) => {
+  jobData?.forEach((job: any) => {
     if (!job) {
       return;
     }
-    const jobId = job.id;
-    const jobTitle = job.title;
+    const jobId = job?.id;
+    const jobTitle = job?.title;
 
-    job.matching_candidates.forEach((matchingCandidate: any) => {
-      const matchedCandidate = candidateData.filter(
-        (candidate: any) => candidate.user_id === matchingCandidate.id
+    job?.matching_candidates?.forEach((matchingCandidate: any) => {
+      const matchedCandidate = candidateData?.filter(
+        (candidate: any) => candidate?.user_id === matchingCandidate?.id
       );
 
       if (matchedCandidate) {
-        const candidateId = matchedCandidate[0].user_id;
-        const candidateFirstName = matchedCandidate[0].first_name;
-        const candidateLastName = matchedCandidate[0].last_name;
-        const candidateScore = matchingCandidate.score;
+        const candidateId = matchedCandidate[0]?.user_id;
+        const candidateFirstName = matchedCandidate[0]?.first_name;
+        const candidateLastName = matchedCandidate[0]?.last_name;
+        const candidateScore = matchingCandidate?.score;
 
         const resultObject = {
           jobId,
