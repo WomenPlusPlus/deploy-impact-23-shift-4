6c842b14edb83a568bfa7bef998f407759f2e100
Alba Romero <albaromero22@gmail.com>
2023-10-23 21:49:54 +0200

    feat: add api route for matching

--- server/model/vectorizer.py
+++ server/model/vectorizer.py
@@ -4,27 +4,33 @@ import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 import dill
 
+
 def tokens(skill):
-  init_list = skill.lower().split(' ')
-  if len(init_list) > 1:
-    init_list.append('_'.join(skill.lower().split(" ")))
+    init_list = skill.lower().split(" ")
+    if len(init_list) > 1:
+        init_list.append("_".join(skill.lower().split(" ")))
 
-  final_list = [token for token in init_list if token not in string.punctuation and not token.isnumeric()]
-    # print(final_list)
-  return final_list
+    final_list = [
+        token
+        for token in init_list
+        if token not in string.punctuation and not token.isnumeric()
+    ]
 
-if __name__ == '__main__':
-    skills = requests.get('http://localhost:5001/api/get_all_skills')
+    return final_list
 
-    skills = [skill['name'] for skill in skills.json()['skills']]
 
-    vectorizer = TfidfVectorizer(tokenizer=tokens)
+if __name__ == "__main__":
+    skills = requests.get("http://localhost:5001/api/get_all_skills")
+
+    skills = [skill["name"] for skill in skills.json()["skills"]]
+
+    vectorizer = TfidfVectorizer(tokenizer=tokens, token_pattern=None)
 
     tfidf = vectorizer.fit_transform(skills)
-    file_path = (os.path.dirname(__file__))
+    file_path = os.path.dirname(__file__)
 
     # print(file_path)
-    # path = 
+    # path =
 
-    with open(f'{file_path}/vectorizer.pkl','wb') as file:
+    with open(f"{file_path}/vectorizer.pkl", "wb") as file:
         dill.dump(vectorizer, file)
