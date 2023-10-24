1370cc55a67e6d23f59581efbc7d2806ee2d9b22
Alba Romero <albaromero22@gmail.com>
2023-10-24 19:48:18 +0200

    integrate matching candidates in FE

--- server/routes/jobs/add_job.py
+++ server/routes/jobs/add_job.py
@@ -6,7 +6,11 @@ def add_job_route(Jobs, db):
 
     @add_job_bp.route("/api/add_job", methods=["POST"])
     def add_job():
-        """ """
+        """
+        Add a new job to the database
+        """
+        id = db.Column(db.Integer, primary_key=True)
+
         if request.method == "POST":
             try:
                 data = request.get_json()
@@ -14,7 +18,19 @@ def add_job_route(Jobs, db):
                 print(new_job.to_dict())
                 db.session.add(new_job)
                 db.session.commit()
-                return jsonify({"message": "Job added successfully"})
+
+                new_job_id = new_job.to_dict()["id"]
+                print(new_job_id, "AAA")
+
+                return (
+                    jsonify(
+                        {
+                            "message": "Job added successfully",
+                            "job_id": new_job_id,
+                        }
+                    ),
+                    200,
+                )
             except Exception as e:
                 print(e)
                 return jsonify({"message": "Error adding job"}), 500
